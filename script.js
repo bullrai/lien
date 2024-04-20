8317084026daa08c9fd931df1f464fc44da1bcff
bullrai <bullrai95@gmail.com>
2024-04-20 09:02:31 +0200

    update code
    première vrai version

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.getElementById('monBouton').addEventListener('click', function() {
-    this.style.backgroundColor = this.style.backgroundColor === '#0056b3' ? '#007bff' : '#0056b3';
+document.getElementById('button').addEventListener('click', function() {
+    this.style.backgroundColor = this.style.backgroundColor === '#3e61df' ? '#4b97e3' : '#3e61df';
 });
